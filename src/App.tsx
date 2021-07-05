7481da792091927a2b434bf5a7baded1d2a72ef6
Sumanta Mukherjee <sumanta.mukherjee.kat@gmail.com>
2021-07-06 00:37:11 +0530

    frontend setup

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,17 @@
 import Counter from './Counter';
 
 const App = () => {
+    const num = 'ss';
+
     return (
         <>
-            <h1>React Typescript webpack starter template. {process.env.NODE_ENV} - {process.env.name}</h1>
+            <h1>
+                React Typescript webpack starter template.{' '}
+                {process.env.NODE_ENV} - {process.env.name}
+            </h1>
             <Counter />
         </>
-    )
-}
+    );
+};
 
 export default App;
